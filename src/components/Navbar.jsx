e2381ed6ec6962ad173bb5d3cc0ed0e39fb49d80
Abdulaziz <abuxyziabd@gmail.com>
2026-05-01 23:24:34 +0500

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,20 +12,10 @@ const Navbar = () => {
 				</NavLink>
 				<ul className="nav-links">
 					<li>
-						<NavLink
-							to="/"
-							className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
-						>
-							Home
-						</NavLink>
+						<NavLink to="/">Home</NavLink>
 					</li>
 					<li>
-						<NavLink
-							to="/contact"
-							className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
-						>
-							Contact
-						</NavLink>
+						<NavLink to="/contact">Contact</NavLink>
 					</li>
 				</ul>
 			</div>
